c1657a1fa65fa2d1f5b29308605fd004a18bdb57
NazarKubynets <evrik1330@gmail.com>
2022-10-27 11:50:32 +0300

    Posts up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ComponentsArr from './Components/ComponentsArr';
+import Number from './Components/Number';
+import ComponentPosts from './Components/Posts/ComponentPosts';
+import PostsOne from './Components/PostsOne/PostsOne';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <PostsOne/> */}
+      {/* <ComponentPosts/> */}
+      {/* <ComponentsArr/> */}
+      {/* <Number/> */}
     </div>
   );
 }
 
 export default App;
+
+// масив і внизу виклик функції, напиши функцію в якій по id буде мінятись значення в selected з false на true того айтема, яку id передають
